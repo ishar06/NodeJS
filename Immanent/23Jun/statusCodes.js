4b8541e2cc7ef472a183562ce6d391ae1c761c11
Ishardeep Singh <singhishardeep06@gmail.com>
2025-06-23 12:04:41 +0530

    Status Codes

---
+++ Immanent/23Jun/statusCodes.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const fs = require("fs");
+const users = require("./MOCK_DATA.json");
+const PORT = 8000;
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.get("/users", (req, res) => {
+   const html = `
+   <ul>
+   ${users.map((user => `<li>${user.first_name}</li>`)).join("")}
+   </ul>
+   `
+   return res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+   return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+   const id = Number(req.params.id);
+   const user = users.find((user) => id === user.id);
+   if (!user){
+    return res.status(404).json({error: "User Not Found"});
+   }
+   return res.json(user);
+});
+
+app.route("/api/users/:id")
+   .get((req, res) => {
+       const id = Number(req.params.id);
+       const user = users.find((user) => id === user.id);
+       return res.json(user);
+   })
+   .patch((req, res) => {
+       return res.status(501).json({ status: "pending" });
+   })
+   .delete((req, res) => {
+       return res.status(501).json({ status: "pending" });
+   });
+
+
+app.post("/api/users", (req, res) => {
+   const body = req.body;
+   if (!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title ){
+    return res.status(400).json({status:"All fields are required"});
+   }
+   users.push({ ...body, id: users.length + 1 });
+   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data) => {
+       if (err) {
+           return res.status(500).json({ status: "error", error: err.message });
+       }
+       return res.status(201).json({ status: "Success", id: users.length });
+   });
+});
+
+
+app.listen(PORT, () => console.log("Server started at http://localhost:" + PORT));
